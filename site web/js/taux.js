96440d43e660e4a5e3c113ef85ac3bbf398a8bb2
Maxime <maxime.doutr@sfr.fr>
2019-12-29 21:02:04 +0100

    Modif taux de satisfaction

--- site web/js/taux.js
+++ site web/js/taux.js
@@ -2,10 +2,15 @@ $(function() {
 
   var s = document.getElementById('salle1');
   var monTexte = s.innerText || s.textContent;
+
   fetch("http://localhost:8080/api/avis/byTauxSatisfaction/Alcatraz")
-  .then(response => response.json(){
-    $("#taux1").append(response.note);
-  })
-  .catch(error => alert("Erreur : " + error));
+      .then(response => response.json())
+      .then(response => {
+          response.forEach(function(element){
+            $("#taux1").append(element['1'] + "%");
+
+          });
+      })
+      .catch(error => alert("Erreur : " + error));
 
 });
