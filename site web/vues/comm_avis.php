7310798a5790089aba408b3274fc2033b8b5934b
Maxime <maxime.doutr@sfr.fr>
2020-02-15 18:03:04 +0100

    Système aléatoire pour nombre d'affichage de photo et nom avec bdd

--- site web/vues/comm_avis.php
+++ site web/vues/comm_avis.php
@@ -1,3 +1,23 @@
+<?php
+    //Variables initialisé pour le container photos et avis
+    $nbavis = 0;
+    $notemini = 0;
+    $nbAvisAlea = 0;
+    $nbphotos = 0;
+    $nbPhotosAlea = 0;
+    $nbMaxPhotos = 0;
+
+    //Récupération de la configuration en json
+    $configurations = file_get_contents("http://localhost:8080/api/configurations");
+    $parsee = json_decode($configurations, true);
+
+    //Parcour le resultat en json de la configuration
+    foreach ($parsee as $config){
+      $nbavis = $config['nbAvis'];
+      $notemini = $config['noteMin'];
+      $nbphotos = $config['nbCommentaire'];
+    }
+?>
 <div class="comm_avis" id="comm_avis">
   <div class="container">
     <div class="row">
@@ -5,31 +25,33 @@
         <h2 class="underline text-uppercase">Photos</h2>
       </div>
     </div>
-    <div class="row">
+    <div class="row mb-3">
+
       <?php
-      //Variables initialisé pour le container photos et avis
-      $nbavis = 0;
-      $notemini = 0;
-      $nbAvisAlea = 0;
-      $nbphotos = 0;
+      //Récupération du nombre d'image en json
+      $nbPhotosTotal = file_get_contents("http://localhost:8080/api/images/countPhotos");
+      $nbMaxPhotos = json_decode($nbPhotosTotal, true);
 
-      //Récupération de la configuration en json
-      $configurations = file_get_contents("http://localhost:8080/api/configurations");
-      $parsee = json_decode($configurations, true);
-
-      //Parcour le resultat en json de la configuration
-      foreach ($parsee as $config){
-        $nbavis = $config['nbAvis'];
-        $notemini = $config['noteMin'];
-        $nbphotos = $config['nbCommentaire'];
+      //Parcour le tableau retournée
+      foreach ($nbMaxPhotos as $nombre) {
+        $nbMaxPhotos = $nombre['nbPhotos'];
       }
+
+      //Récupération du nom des photos en json
+      $lesPhotos = file_get_contents("http://localhost:8080/api/themes/getImagesSalle");
+      $nomPhotos = json_decode($lesPhotos, true);
 
         //Affichage des photos
         for ($o=0; $o < $nbphotos; $o++) {
+
+          $nbPhotosAlea = rand(0, $nbMaxPhotos-1); //nombre aléatoire photos
+
           echo '
-          <div class="gallery_comm col-lg-4 col-md-4 col-sm-4 col-xs-4 filter hdpe">
-            <img src="css/images/room.jpg" class="img-responsive">
-          </div>';
+          <div class="col-md-4" id="border_salle">
+                <div class="card">
+                    <img src="css/images/'. $nomPhotos[$nbPhotosAlea]['nom'] .'" class="card-img-top affiche_photos">
+                </div>
+            </div>';
         }
       ?>
 
@@ -54,9 +76,9 @@
 
           $inc = 1;
 
-          for ($j=1; $j < $nbMaxAvis; $j++) {
+          for ($j=0; $j < $nbMaxAvis; $j++) {
 
-            $nbAvisAlea = rand(1, $nbMaxAvis-1); //nombre aléatoire
+            $nbAvisAlea = rand(0, $nbMaxAvis-1); //nombre aléatoire avis
 
             if ($inc > $nbavis) { //Force le sortie de la boucle
               break;
